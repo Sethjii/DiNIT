3d96843b92823f42222cf752fa122ad29e5dd387
harshita3002 <113182231+harshita3002@users.noreply.github.com>
2023-01-20 15:18:42 +0530

    Add files via upload

---
+++ DiNITgit/src/pages/ViewCart.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react";
+import styles from "./ViewCart.module.css";
+
+const ViewCart: FunctionComponent = () => {
+  return (
+    <div className={styles.viewCart}>
+      <img className={styles.backSymbolIcon} alt="" src="../back-symbol.svg" />
+      <img
+        className={styles.continueOptionIcon}
+        alt=""
+        src="../continue-option.svg"
+      />
+      <div className={styles.statusBar}>
+        <div className={styles.time}>9:27</div>
+        <img className={styles.wifiIcon} alt="" src="../wifi.svg" />
+        <img className={styles.cellIcon} alt="" src="../cell.svg" />
+        <div className={styles.battery}>
+          <div className={styles.border} />
+          <img className={styles.capIcon} alt="" src="../cap.svg" />
+          <div className={styles.capacity} />
+        </div>
+      </div>
+      <div className={styles.orderSuccessful}>Order Successful :)</div>
+      <img className={styles.phoneMenuIcon} alt="" src="../phone-menu.svg" />
+    </div>
+  );
+};
+
+export default ViewCart;
